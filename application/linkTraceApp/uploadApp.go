1a3867e0fd6bfe71dc4dab394365262ba8bcead1
steden <1470804@qq.com>
2024-01-12 21:19:32 +0800

    打开记录链路信息

--- application/linkTraceApp/uploadApp.go
+++ application/linkTraceApp/uploadApp.go
@@ -5,11 +5,16 @@ import (
 	"fops/application/linkTraceApp/request"
 	"fops/domain/linkTrace"
 	"github.com/farseer-go/fs/exception"
+	"github.com/farseer-go/fs/trace"
 )
 
 // Upload 上传链路记录
 // @post upload
 func Upload(req request.UploadRequest, linkTraceRepository linkTrace.Repository) {
+	if t := trace.CurTraceContext.Get(); t != nil {
+		t.Ignore()
+	}
+
 	err := linkTraceRepository.Save(req.List)
 	exception.ThrowWebExceptionError(403, err)
 }

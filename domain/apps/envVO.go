c658d3a79e8e1327107374692098a3052a81e609
steden <1470804@qq.com>
2024-03-24 18:21:25 +0800

    添加存放工作流文件的目录地址

--- domain/apps/envVO.go
+++ domain/apps/envVO.go
@@ -11,12 +11,13 @@ const (
 	WithJsonPath     = "/var/lib/fops/dist/with.json"     // with.json文件位置
 	KubeRoot         = "/var/lib/fops/kube/"              // kubectlConfig配置
 	NpmModulesRoot   = "/var/lib/fops/npm"                // NpmModules
-	DistRoot         = "/var/lib/fops/dist/"              //  编译保存的根目录
+	DistRoot         = "/var/lib/fops/dist/"              // 编译保存的根目录
 	GitRoot          = "/var/lib/fops/git/"               // GIT根目录
 	DockerfilePath   = "/var/lib/fops/dist/Dockerfile"    // Dockerfile文件地址
 	DockerIgnorePath = "/var/lib/fops/dist/.dockerignore" // DockerIgnore文件地址
 	ShellRoot        = "/var/lib/fops/shell/"             // 生成Shell脚本的存放路径
 	ActionsRoot      = "/var/lib/fops/actions/"           // 执行Actions的缓存目录
+	WorkflowsRoot    = "/var/lib/fops/workflows/"         // 存放工作流文件的目录
 )
 
 // InitFopsDir 初始化目录
@@ -28,6 +29,7 @@ func InitFopsDir() {
 	file.CreateDir766(GitRoot)
 	file.CreateDir766(ShellRoot)
 	file.CreateDir766(ActionsRoot)
+	file.CreateDir766(WorkflowsRoot)
 }
 
 // EnvVO 构建时的环境变量

a25de99165780c0dcb24b681b655275e06128631
steden <1470804@qq.com>
2024-05-09 17:36:28 +0800

    修改镜像名称生成规则

--- pushLocalHub.sh
+++ pushLocalHub.sh
@@ -12,6 +12,7 @@ docker push hub.fsgit.cc/fops:dev && docker rmi hub.fsgit.cc/fops:dev
 # docker
 docker service rm fops
 docker service create --name fops --replicas 1 -d --network=net \
+ --with-registry-auth \
 --constraint node.role==manager \
 --mount type=bind,src=/etc/localtime,dst=/etc/localtime \
 --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
@@ -20,6 +21,6 @@ docker service create --name fops --replicas 1 -d --network=net \
 -l "traefik.http.routers.fops.entrypoints=websecure" \
 -l "traefik.http.routers.fops.tls=true" \
 -l "traefik.http.services.fops.loadbalancer.server.port=8889" \
-steden88/fops:2.0.beta3
+hub.fsgit.cc/fops:dev
 
 docker run --name fops  -d --network=host -e "Database_default=DataType=mysql,PoolMaxSize=50,PoolMinSize=1,ConnectionString=root:harlen@tcp(host.docker.internal:3307)/fops2?charset=utf8&parseTime=True&loc=Local" -e "Redis_default=Server=host.docker.internal:6379,DB=13,Password=,ConnectTimeout=600000,SyncTimeout=10000,ResponseTimeout=10000" -e "FOPS_ConnString=DataType=clickhouse,PoolMaxSize=50,PoolMinSize=1,ConnectionString=clickhouse://default:@host.docker.internal:9000/linkTrace?dial_timeout=10s&read_timeout=20s" hub.fsgit.cc/fops:483

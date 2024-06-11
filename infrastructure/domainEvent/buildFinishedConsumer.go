8ec1d804f16d153fc028a2ab88d506b21b7fddb7
steden <1470804@qq.com>
2024-06-11 13:20:24 +0800

    修改字段长度

--- infrastructure/domainEvent/buildFinishedConsumer.go
+++ infrastructure/domainEvent/buildFinishedConsumer.go
@@ -1,12 +1,10 @@
 package domainEvent
 
 import (
-	"encoding/json"
 	"fops/domain/apps"
 	"fops/domain/apps/event"
 	"github.com/farseer-go/fs/container"
 	"github.com/farseer-go/fs/core"
-	"github.com/farseer-go/fs/flog"
 )
 
 // BuildFinishedConsumer 构建完成事件：更新应用的版本信息
@@ -14,13 +12,9 @@ func BuildFinishedConsumer(message any, _ core.EventArgs) {
 	appsRepository := container.Resolve[apps.Repository]()
 	buildFinishedEvent := message.(event.BuildFinishedEvent)
 
-	flog.Infof("收到构建完成事件消息：IsSuccess=%v,ClusterId=%d,BuildId=%d", buildFinishedEvent.IsSuccess, buildFinishedEvent.ClusterId, buildFinishedEvent.BuildId)
-
 	// 更新项目的版本信息
 	appsDO := appsRepository.ToEntity(buildFinishedEvent.AppName)
 	appsDO.UpdateBuildVer(buildFinishedEvent.IsSuccess, buildFinishedEvent.ClusterId, buildFinishedEvent.BuildId)
 
-	marshal, _ := json.Marshal(appsDO.ClusterVer)
-	flog.Infof("更新后的集群map=%s", string(marshal))
 	_, _ = appsRepository.UpdateClusterVer(buildFinishedEvent.AppName, appsDO.ClusterVer)
 }

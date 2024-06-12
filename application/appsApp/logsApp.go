131e4308f3c5873e6ea961d640e4fbc8b2ab132a
steden <1470804@qq.com>
2024-06-12 18:02:26 +0800

    清除控制台日志颜色

--- application/appsApp/logsApp.go
+++ application/appsApp/logsApp.go
@@ -3,11 +3,14 @@ package appsApp
 
 import (
 	"fops/domain/apps"
+	"github.com/farseer-go/fs/flog"
 )
 
 // DockerSwarm 获取容器日志
 // @post logs/dockerSwarm
 // @filter application.Jwt
 func DockerSwarm(appName string, tailCount int, appsIDockerSwarmDevice apps.IDockerSwarmDevice) string {
-	return appsIDockerSwarmDevice.Logs(appName, tailCount).ToString("\r\n")
+	logs := appsIDockerSwarmDevice.Logs(appName, tailCount).ToString("\r\n")
+	logs = flog.ClearColor(logs)
+	return logs
 }

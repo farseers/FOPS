6affe522b6d358880bd8241528706975620eb988
steden <1470804@qq.com>
2024-06-12 22:03:59 +0800

    修改样式

--- interfaces/job/collectsClusterJob.go
+++ interfaces/job/collectsClusterJob.go
@@ -12,8 +12,14 @@ import (
 func CollectsClusterJob(*tasks.TaskContext) {
 	dockerSwarmDevice := container.Resolve[apps.IDockerSwarmDevice]()
 	serviceList := dockerSwarmDevice.ServiceList()
+	// 移除标题
+	serviceList.RemoveAt(0)
 	serviceList.Foreach(func(service *string) {
-		sers := strings.Split(*service, "\t")
-		fmt.Println(sers[0])
+		// 移除容器ID
+		*service = strings.TrimSpace((*service)[12:])
+		*service = strings.Replace(*service, "\t", "", -1)
+		sers := strings.Split(*service, " ")
+		fmt.Println(len(sers))
+		fmt.Println(strings.Join(sers, "|"))
 	})
 }

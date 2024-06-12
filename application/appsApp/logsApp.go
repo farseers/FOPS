cc94d914afb246034f7bbbe166367d3f5c2af67e
steden <1470804@qq.com>
2024-06-12 17:53:51 +0800

    添加获取容器日志接口

--- application/appsApp/logsApp.go
+++ application/appsApp/logsApp.go
@@ -3,12 +3,11 @@ package appsApp
 
 import (
 	"fops/domain/apps"
-	"github.com/farseer-go/collections"
 )
 
 // DockerSwarm 获取容器日志
 // @post logs/dockerSwarm
 // @filter application.Jwt
-func DockerSwarm(appName string, tailCount int, appsIDockerSwarmDevice apps.IDockerSwarmDevice) collections.List[string] {
-	return appsIDockerSwarmDevice.Logs(appName, tailCount)
+func DockerSwarm(appName string, tailCount int, appsIDockerSwarmDevice apps.IDockerSwarmDevice) string {
+	return appsIDockerSwarmDevice.Logs(appName, tailCount).ToString("\r\n")
 }

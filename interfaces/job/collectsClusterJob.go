80cff0aafb6fb48ead716e1394d60b58fc27e231
steden <1470804@qq.com>
2024-06-12 21:57:22 +0800

    修改样式

--- interfaces/job/collectsClusterJob.go
+++ interfaces/job/collectsClusterJob.go
@@ -5,14 +5,15 @@ import (
 	"fops/domain/apps"
 	"github.com/farseer-go/fs/container"
 	"github.com/farseer-go/tasks"
+	"strings"
 )
 
 // CollectsClusterJob 3秒收集一次Docker集群信息
 func CollectsClusterJob(*tasks.TaskContext) {
 	dockerSwarmDevice := container.Resolve[apps.IDockerSwarmDevice]()
 	serviceList := dockerSwarmDevice.ServiceList()
-	if serviceList.Count() == 0 {
-		fmt.Println("没有获取到任何服务")
-	}
-	fmt.Println(serviceList.ToString("\r\n"))
+	serviceList.Foreach(func(service *string) {
+		sers := strings.Split(*service, "\t")
+		fmt.Println(sers[0])
+	})
 }

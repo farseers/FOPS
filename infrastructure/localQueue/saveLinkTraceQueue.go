e0825e160222f48a8cef9a27d63ca1e6dc7bec54
steden <1470804@qq.com>
2024-07-01 16:37:05 +0800

    优化

--- infrastructure/localQueue/saveLinkTraceQueue.go
+++ infrastructure/localQueue/saveLinkTraceQueue.go
@@ -3,6 +3,7 @@ package localQueue
 import (
 	"github.com/farseer-go/collections"
 	"github.com/farseer-go/fs/trace"
+	linkTraceCom "github.com/farseer-go/linkTrace"
 )
 
 func SaveLinkTraceQueue(subscribeName string, lstMessage collections.ListAny, remainingCount int) {
@@ -10,10 +11,10 @@ func SaveLinkTraceQueue(subscribeName string, lstMessage collections.ListAny, re
 		traceContext.Ignore()
 	}
 
-	//lst := collections.NewList[linkTraceCom.TraceContext]()
-	//lstMessage.Foreach(func(item *any) {
-	//	lst.Add((*item).(linkTraceCom.TraceContext))
-	//})
+	lst := collections.NewList[linkTraceCom.TraceContext]()
+	lstMessage.Foreach(func(item *any) {
+		lst.Add((*item).(linkTraceCom.TraceContext))
+	})
 	//
 	//err := container.Resolve[linkTrace.Repository]().Save(lst)
 	//flog.ErrorIfExists(err)

a4254882d4cb11ae161dfa39f33eb66315e241d9
steden <1470804@qq.com>
2024-07-02 13:31:57 +0800

    如果服务不存在，则添加到列表中，用于更新到数据库中，指明服务的实例为0

--- interfaces/job/collectsClusterJob.go
+++ interfaces/job/collectsClusterJob.go
@@ -26,9 +26,25 @@ func CollectsClusterJob(*tasks.TaskContext) {
 
 	// 收集所有服务的运行情况
 	serviceList := dockerSwarmDevice.ServiceList()
+	// 如果服务不存在，则添加到列表中，用于更新到数据库中，指明服务的实例为0
+	lstApp := appsRepository.ToList()
+	lstApp.Foreach(func(appDO *apps.DomainObject) {
+		if !serviceList.Where(func(service apps.DockerServiceVO) bool {
+			return appDO.AppName == service.Name
+		}).Any() {
+			serviceList.Add(apps.DockerServiceVO{
+				Name:      appDO.AppName,
+				Instances: 0,
+				Image:     "",
+				Replicas:  appDO.DockerReplicas,
+			})
+		}
+	})
 
 	container.Resolve[core.ITransaction]("default").Transaction(func() {
+		// 更新集群节点信息
 		appsRepository.UpdateClusterNode(nodeList)
+		// 更新服务运行情况
 		if serviceList.Count() > 0 {
 			_, _ = appsRepository.UpdateInsReplicas(serviceList)
 		}

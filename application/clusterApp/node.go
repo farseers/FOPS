5a99f24ef21b5a9c36d7cd158066aee66ad364d1
steden <1470804@qq.com>
2024-10-21 10:31:10 +0800

    过滤掉不健康的节点实例详情

--- application/clusterApp/node.go
+++ application/clusterApp/node.go
@@ -13,8 +13,5 @@ import (
 // @filter application.Jwt
 func NodeList(clusterNodeRepository clusterNode.Repository) collections.List[docker.DockerNodeVO] {
 	lst := clusterNodeRepository.GetClusterNodeList()
-	lst.Foreach(func(item *docker.DockerNodeVO) {
-		item.IsHealth = item.Status == "Ready" && item.Availability == "Active"
-	})
 	return lst
 }

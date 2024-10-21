dc96ba6145775ea219c1d0d11e33b15b34de8814
steden <1470804@qq.com>
2024-10-21 22:29:33 +0800

    构建异常时，重启构建环境

--- infrastructure/device/gitDevice.go
+++ infrastructure/device/gitDevice.go
@@ -20,23 +20,28 @@ type gitDevice struct {
 func (receiver *gitDevice) PullWorkflows(ctx context.Context, gitPath, branch string, gitRemote string, progress chan string) bool {
 	if !file.IsExists(gitPath) {
 		file.CreateDir766(gitPath)
-		exec.RunShellContext(ctx, "git init", progress, nil, gitPath, true)
-		exec.RunShellContext(ctx, fmt.Sprintf("git remote add -f origin %s", gitRemote), progress, nil, gitPath, true)
-		exec.RunShellContext(ctx, "git config core.sparsecheckout true", progress, nil, gitPath, true)
-		exec.RunShellContext(ctx, "echo .fops/workflows/ >> .git/info/sparse-checkout", progress, nil, gitPath, true)
+		exec.RunShell("git init", progress, nil, gitPath, true)
+		exec.RunShell(fmt.Sprintf("git remote add -f origin %s", gitRemote), progress, nil, gitPath, true)
+		exec.RunShell("git config core.sparsecheckout true", progress, nil, gitPath, true)
+		exec.RunShell("echo .fops/workflows/ >> .git/info/sparse-checkout", progress, nil, gitPath, true)
 	}
 
 	var exitCode int
 	for i := 0; i < 3; i++ {
-		if exitCode = exec.RunShell(fmt.Sprintf("git pull origin %s", branch), progress, nil, gitPath, true); exitCode == 0 {
-			break
-		}
-		if i == 2 {
+		select {
+		case <-ctx.Done():
 			progress <- "同步工作流文件失败，停止构建"
-		} else {
-			progress <- "拉取失败，正在尝试重新拉取"
+			return false
+		default:
+			if exitCode = exec.RunShellContext(ctx, fmt.Sprintf("git pull origin %s", branch), progress, nil, gitPath, true); exitCode == 0 {
+				break
+			}
+			if i == 2 {
+				progress <- "同步工作流文件失败，停止构建"
+			} else {
+				progress <- "拉取失败，正在尝试重新拉取"
+			}
 		}
-
 	}
 	return exitCode == 0
 }

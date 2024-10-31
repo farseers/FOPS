f823a5b269790e010185df2a59c23bde7d70865d
steden <1470804@qq.com>
2024-10-31 13:27:52 +0800

    当应用出错时，显示所有日志

--- application/appsApp/logsApp.go
+++ application/appsApp/logsApp.go
@@ -3,6 +3,7 @@ package appsApp
 
 import (
 	"fops/application/appsApp/response"
+
 	"github.com/farseer-go/collections"
 	"github.com/farseer-go/docker"
 	"github.com/farseer-go/fs/flog"
@@ -27,6 +28,9 @@ func DockerSwarm(appName string, tailCount int) collections.List[response.Docker
 	lst.Foreach(func(item *docker.ServicePsVO) {
 		// 通过容器id获取日志
 		logs, _ := client.Service.Logs(item.ServiceId, tailCount)
+		if logs.Count() < 2 {
+			return
+		}
 		// 有错误时，则通过docker inspect r6r8uboagmln 获取错误详情
 		if item.Error != "" {
 			containerInspectJson, _ := client.Container.InspectByServiceId(item.ServiceId)
@@ -40,5 +44,27 @@ func DockerSwarm(appName string, tailCount int) collections.List[response.Docker
 			Log:         flog.ClearColor(logs.ToString("\r\n")),
 		})
 	})
+
+	// 没有取到日志时，取全部
+	if lst.Count() == 0 {
+		var image string
+		inspect, _ := client.Service.Inspect(appName)
+		if len(inspect) > 0 {
+			image = inspect[0].Spec.TaskTemplate.ContainerSpec.Image
+		}
+		logs, _ := client.Service.Logs(appName, tailCount*2)
+		rsp.Add(response.DockerSwarmResponse{
+			ServicePsVO: docker.ServicePsVO{
+				ServiceId: appName,
+				Name:      appName,
+				Image:     image,
+				Node:      "all",
+				State:     "所有日志",
+				StateInfo: "所有日志",
+				Error:     "",
+			},
+			Log: flog.ClearColor(logs.ToString("\r\n")),
+		})
+	}
 	return rsp
 }

97a50214351b1cdb8359388d4dcf8563d16df4ee
steden <1470804@qq.com>
2024-11-27 23:44:01 +0800

    修改样式

--- wwwsrc/src/views/fops/build/indexApp.vue
+++ wwwsrc/src/views/fops/build/indexApp.vue
@@ -93,7 +93,7 @@
             <h3 style="padding: 5px;">构建队列 </h3>
             <template v-if="state.tableLogData.data.length > 0">
               <el-table  :data="state.tableLogData.data" v-loading="state.tableLogData.loading" style="width: 100%;background: #ffffff;" :cell-style="{padding:'2px 0'}">
-                <el-table-column prop="FinishAt" width="140" label="构建时间"></el-table-column>
+                <el-table-column prop="FinishAt" width="130" label="构建时间"></el-table-column>
                 <el-table-column label="应用名称" show-overflow-tooltip>
                   <template #default="scope">
                     <el-tag v-if="scope.row.Status==0" size="small" type="info">未开始</el-tag>
@@ -101,7 +101,7 @@
                     <el-tag v-if="scope.row.Status==2 && scope.row.IsSuccess == true" size="small" type="success">成功</el-tag>
                     <el-tag v-else-if="scope.row.Status==2 && scope.row.IsSuccess == false" size="small" type="danger">失败</el-tag>
                     <el-tag size="small" type="info">{{ scope.row.WorkflowsName }}</el-tag>
-                    <el-tag v-if="scope.row.BranchName !=''" size="small" type="info">{{ scope.row.BranchName }}</el-tag>
+                    <el-tag v-if="scope.row.BranchName !=''" size="small">{{ scope.row.BranchName }}</el-tag>
                     <span style="margin-left: 5px ">{{ scope.row.AppName }}:{{ scope.row.BuildNumber }}</span>
                   </template>
                 </el-table-column>

79c47c7bb078ba8e5e2f8f5423d4ba2f4d00eee8
steden <1470804@qq.com>
2025-03-03 20:04:43 +0800

    修改字段

--- infrastructure/repository/context/chContext.go
+++ infrastructure/repository/context/chContext.go
@@ -14,7 +14,7 @@ type chContext struct {
 	data.IInternalContext
 	TraceContextView data.TableSet[model.TraceContextViewPO] `data:"name=v_link_trace;migrate"`
 	TraceContext     data.TableSet[model.TraceContextPO]     `data:"name=link_trace;migrate=ReplacingMergeTree() ORDER BY (trace_type,app_name,parent_app_name,app_ip,trace_id,start_ts) PARTITION BY (trace_type)"`
-	TraceDetail      data.TableSet[model.TraceDetailPO]      `data:"name=trace_detail;migrate=ReplacingMergeTree() ORDER BY (app_name,parent_app_name,app_ip,db_name,table_name,trace_id,start_ts,detail_id) PARTITION BY (app_name)"`
+	TraceDetail      data.TableSet[model.TraceDetailPO]      `data:"name=trace_detail;migrate=ReplacingMergeTree() ORDER BY (app_name,parent_app_name,app_ip,trace_id,start_ts,detail_id) PARTITION BY (call_type)"`
 	LogData          data.TableSet[model.LogDataPO]          `data:"name=log_data;migrate=ReplacingMergeTree() ORDER BY (app_name,component,log_level,app_ip,trace_id,log_id,create_at) PARTITION BY (app_name)"`
 	Visits           data.TableSet[model.VisitsPO]           `data:"name=visits;migrate=ReplacingMergeTree() ORDER BY (create_at,app_name,visits_node_prefix,visits_node,trace_type) PARTITION BY toYYYYMM(create_at)"`
 	MonitorData      data.TableSet[model.MonitorDataPO]      `data:"name=monitor_data;migrate=ReplacingMergeTree() ORDER BY (app_name,create_at) PARTITION BY (app_name)"`

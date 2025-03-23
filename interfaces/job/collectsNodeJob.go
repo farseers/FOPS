693e58d400ab5afa45a3648d0fcb86f8afc0d507
steden <1470804@qq.com>
2025-03-23 17:40:47 +0800

    format

--- interfaces/job/collectsNodeJob.go
+++ interfaces/job/collectsNodeJob.go
@@ -25,16 +25,16 @@ func CollectsNodeJob(*tasks.TaskContext) {
 		// 通过ps节点信息，拿到节点的资源信息
 		dockerNode := dockerClient.Node.Info(node.NodeName)
 		if dockerNode.IP == "" {
-			flog.Warningf("集群节点：%s，没有读取到IP", node.NodeName)
 			return
 		}
 
 		node.IP = dockerNode.IP
+		node.Label = dockerNode.Label
+		// 以下属性暂时不需要，由代理节点获取
 		// node.OS = dockerNode.OS
 		// node.Architecture = dockerNode.Architecture
 		// node.CPUs = dockerNode.CPUs
 		// node.Memory = dockerNode.Memory
-		node.Label = dockerNode.Label
 
 		// 加入到本地列表
 		dockerNodeVO := clusterNode.NodeList.Find(func(dockerItem *docker.DockerNodeVO) bool {
@@ -45,14 +45,15 @@ func CollectsNodeJob(*tasks.TaskContext) {
 			clusterNode.NodeList.Add(*node)
 		} else {
 			dockerNodeVO.IsHealth = node.IsHealth
-			// dockerNodeVO.OS = node.OS
-			// dockerNodeVO.Architecture = node.Architecture
-			// dockerNodeVO.CPUs = node.CPUs
-			// dockerNodeVO.Memory = node.Memory
 			dockerNodeVO.Label = node.Label
 			dockerNodeVO.UpdateAt = time.Now()
 			dockerNodeVO.Status = node.Status
 			dockerNodeVO.Availability = node.Availability
+			// 以下属性暂时不需要，由代理节点获取
+			// dockerNodeVO.OS = node.OS
+			// dockerNodeVO.Architecture = node.Architecture
+			// dockerNodeVO.CPUs = node.CPUs
+			// dockerNodeVO.Memory = node.Memory
 		}
 	})
 

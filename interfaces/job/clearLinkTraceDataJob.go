fde6b17c516ff954e8f76d42196a666487a6896c
steden <1470804@qq.com>
2025-06-07 12:57:11 +0800

    增加自动清除日志功能

--- interfaces/job/clearLinkTraceDataJob.go
+++ interfaces/job/clearLinkTraceDataJob.go
@@ -2,6 +2,7 @@ package job
 
 import (
 	"fops/domain/linkTrace"
+	"fops/domain/logData"
 	"time"
 
 	"github.com/farseer-go/fs/configure"
@@ -12,6 +13,7 @@ import (
 // SyncBackupDataJob 清除链路数据
 func ClearLinkTraceDataJob(*tasks.TaskContext) {
 	linkTraceRepository := container.Resolve[linkTrace.Repository]()
+	logDataRepository := container.Resolve[logData.Repository]()
 	days := configure.GetInt("Fops.LinkTrace.SaveDays")
 	if days <= 0 {
 		return
@@ -21,4 +23,5 @@ func ClearLinkTraceDataJob(*tasks.TaskContext) {
 	// 清除链路数据
 	linkTraceRepository.Delete(time.Now().AddDate(0, 0, days))
 	linkTraceRepository.DeleteSlow(time.Now().AddDate(0, 0, days))
+	logDataRepository.DeleteLog(time.Now().AddDate(0, 0, days))
 }

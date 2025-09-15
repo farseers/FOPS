b4496b1979d29e4080cc37b8ee2f0dbf3603eca8
steden <1470804@qq.com>
2025-09-15 17:25:51 +0800

    实现日志告警

--- infrastructure/localQueue/saveFlogQueue.go
+++ infrastructure/localQueue/saveFlogQueue.go
@@ -2,10 +2,14 @@ package localQueue
 
 import (
 	"fops/domain/logData"
+	"fops/domain/monitor"
+
 	"github.com/farseer-go/collections"
 	"github.com/farseer-go/fs/container"
+	"github.com/farseer-go/fs/core/eumLogLevel"
 	"github.com/farseer-go/fs/flog"
 	"github.com/farseer-go/fs/trace"
+	"github.com/farseer-go/queue"
 )
 
 func SaveFlogQueue(subscribeName string, lstMessage collections.ListAny, remainingCount int) {
@@ -16,6 +20,13 @@ func SaveFlogQueue(subscribeName string, lstMessage collections.ListAny, remaini
 	for _, item := range lstMessage.ToArray() {
 		data := item.(*flog.LogData)
 		lst.Add(*data)
+
+		switch data.LogLevel {
+		case eumLogLevel.Error:
+			queue.Push("monitor", monitor.NewDataEO(data.AppName, "log_error", data.Content))
+		case eumLogLevel.Warning:
+			queue.Push("monitor", monitor.NewDataEO(data.AppName, "log_warning", data.Content))
+		}
 	}
 
 	err := container.Resolve[logData.Repository]().Save(lst)

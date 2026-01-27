a6da56533bc42e4defd8039aae2c46087b0148d1
steden <1470804@qq.com>
2026-01-27 22:01:25 +0800

    优化容器日志

--- application/appsApp/logsApp.go
+++ application/appsApp/logsApp.go
@@ -2,6 +2,7 @@
 package appsApp
 
 import (
+	"fmt"
 	"fops/application/appsApp/response"
 
 	"github.com/farseer-go/collections"
@@ -21,21 +22,22 @@ func DockerSwarm(appName string, tailCount int) collections.List[response.Docker
 		// 通过容器id获取日志
 		logs, _ := client.Service.Logs(item.ServiceTaskId, tailCount)
 		if item.Error != "" {
-			flog.Infof("%s 发现错误日志：%s", item.ServiceTaskId, item.Error)
 			containerInspectJson, _ := client.Container.InspectByServiceId(item.ServiceTaskId)
 			if len(containerInspectJson) > 0 {
 				item.Error = containerInspectJson[0].Status.Err
-				flog.Infof("%s 重新取错误日志：%s", item.ServiceTaskId, item.Error)
 			}
 		}
-
+		serviceLog := logs.First()
 		// 没有取到日志时
-		if logs.Count() < 2 {
-			logs.Clear()
+		if serviceLog.Logs.Count() < 2 {
+			serviceLog.Logs.Clear()
+			item.Tasks.Foreach(func(taskInstanceVO *docker.TaskInstanceVO) {
+				serviceLog.Logs.Add(fmt.Sprintf("%s\t%s\t%s\t%s\t%s", taskInstanceVO.TaskId, taskInstanceVO.Image, taskInstanceVO.Node, taskInstanceVO.State, taskInstanceVO.Error))
+			})
 		}
 		rsp.Add(response.DockerSwarmResponse{
 			ServiceTaskVO: *item,
-			Log:           flog.ClearColor(logs.ToString("\r\n")),
+			Log:           flog.ClearColor(serviceLog.Logs.ToString("\r\n")),
 		})
 	})
 
@@ -43,12 +45,10 @@ func DockerSwarm(appName string, tailCount int) collections.List[response.Docker
 	if rsp.All(func(item response.DockerSwarmResponse) bool {
 		return item.Log == ""
 	}) {
-		flog.Infof("发现没有取到日志")
 		var image string
 		inspect, _ := client.Service.Inspect(appName)
 		if len(inspect) > 0 {
 			image = inspect[0].Spec.TaskTemplate.ContainerSpec.Image
-			flog.Infof("重新获取镜像：%s", image)
 		}
 
 		// 这里取到的是服务日志，即所有容器的日志。需要把他们区分开来
@@ -60,7 +60,7 @@ func DockerSwarm(appName string, tailCount int) collections.List[response.Docker
 				rsp.Add(response.DockerSwarmResponse{
 					ServiceTaskVO: docker.ServiceTaskVO{
 						ServiceTaskId: serviceLogVO.ContainerId,
-						Name:          appName,
+						Name:          serviceLogVO.ServiceName,
 						Image:         image,
 						Node:          serviceLogVO.NodeName,
 						State:         "",
@@ -69,13 +69,15 @@ func DockerSwarm(appName string, tailCount int) collections.List[response.Docker
 					},
 					Log: flog.ClearColor(serviceLogVO.Logs.ToString("\r\n")),
 				})
-				flog.Infof("添加日志：%s,%s", serviceLogVO.ContainerId, serviceLogVO.Logs.ToString("\r\n"))
 			} else {
 				curRsp.Log += flog.ClearColor("\r\n" + serviceLogVO.Logs.ToString("\r\n"))
-				flog.Infof("追加日志：%s,%s", serviceLogVO.ContainerId, serviceLogVO.Logs.ToString("\r\n"))
 			}
 		})
 	}
 
+	// 移除没有日志的项
+	rsp.RemoveAll(func(item response.DockerSwarmResponse) bool {
+		return item.Log == ""
+	})
 	return rsp
 }
